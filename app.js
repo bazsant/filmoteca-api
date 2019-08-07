0d4548c3ba3bf6144f5a42679e4224b6532874e5
Jhones Bazilio dos Santos <jhones.santos@ituran.sp>
2019-08-07 16:45:25 -0300

    adicionando cors

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var infoRouter = require('./routes/info');
-var cors = require('cors'); 
 
 var app = express();
 
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
