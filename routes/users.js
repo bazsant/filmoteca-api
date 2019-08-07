768eeccf8bbc51d5f3929361d9f7de8200e10222
Jhones Bazilio dos Santos <jhones.santos@ituran.sp>
2019-08-07 09:59:25 -0300

    query de create user

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,21 @@ router.get('/', function (req, res, next) {
   })
 });
 
+router.get('/create', function (req, res, next) {
+  db.query(`CREATE TABLE users (
+    id bigserial primary key,
+    name varchar(40) NOT NULL,
+    phone varchar(20) NOT NULL,
+    gender varchar(1) NOT NULL,
+  	email varchar(40) NOT NULL,
+  	birth date NOT NULL
+    );`, [], (err, ret) => {
+      if (err) {
+        res.json({ error: err })
+      } else {
+        res.send(ret)
+      }
+    })
+});
+
 module.exports = router;
